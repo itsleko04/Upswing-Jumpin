158116db97aa76d4571b5c9b1d9f8d5fe74068dc
itsleko04 <lipnitskiyvv@gmail.com>
2026-01-22 19:31:18 +0300

    Пофиксил отображение GameResultView

--- Assets/Scripts/Engine/GameResultView.py
+++ Assets/Scripts/Engine/GameResultView.py
@@ -8,69 +8,59 @@ class GameResultView(arcade.View):
     def __init__(self, application, caption: str, result_message: str = ""):
         super().__init__(application.window)
         self.application = application
-        self.texture = arcade.load_texture("Assets/Sprites/menuBG.jpg")
-        self.settings_list = arcade.SpriteList()
-        self.escape_list = arcade.SpriteList()
-        self.play_list = arcade.SpriteList()
+        self.texture = arcade.load_texture("Assets/Sprites/gameOverBG.jpg")
+        self.to_menu_list = arcade.SpriteList()
+        self.restart_list = arcade.SpriteList()
 
         self.gui_camera = arcade.camera.Camera2D()
         
         self.window.set_caption(caption)
         self.batch = Batch()
-        self.restart_info = arcade.Text(
+        self.result_info = arcade.Text(
             text=result_message,
-            color=arcade.color.WHITE,
+            color=(96, 117, 102),
             x=self.center_x,
-            y=self.center_y,
-            font_size=24,
+            y=self.center_y + self.center_y * 2 / 3,
+            font_size=48,
             anchor_x="center",
             batch=self.batch
         )
 
-    def on_draw(self):
-        self.clear()
-        self.gui_camera.use()
-        self.batch.draw()
-
     def on_show_view(self):
         """Настройка при показе меню"""
-        self.settings_list.clear()
-        self.escape_list.clear()
-        self.play_list.clear()
+        self.to_menu_list.clear()
+        self.restart_list.clear()
 
-        # Кнопка Play
-        play = arcade.Sprite("Assets/Sprites/playBtn.jpg", 0.5)
-        play.center_x = self.application.width // 2
-        play.center_y = 380
-        self.play_list.append(play)
+        y_offset = 100
 
-        # Кнопка Escape
-        escape = arcade.Sprite("Assets/Sprites/exitBtn.jpg", 0.5)
-        escape.center_x = self.application.width // 2
-        escape.center_y = 130
-        self.escape_list.append(escape)
+        restart = arcade.Sprite("Assets/Sprites/gameOverRestartBtn.jpg", 0.5)
+        restart.center_x = self.application.width // 2
+        restart.center_y = self.application.height // 2 - y_offset
+        self.restart_list.append(restart)
+
+        to_menu = arcade.Sprite("Assets/Sprites/showMenuBtn.jpg", 0.5)
+        to_menu.center_x = self.application.width // 2
+        to_menu.center_y = self.application.height // 2 - 120 - y_offset
+        self.to_menu_list.append(to_menu)
 
     def restart(self):
         self.application.start_level(self.application.last_started_level)
 
     def on_draw(self):
         self.clear()
-        # Рисуем фон
+        self.gui_camera.use()
         arcade.draw_texture_rect(self.texture, arcade.rect.XYWH(self.application.width // 2,
                 self.application.height // 2, self.application.width, self.application.height))
-        # Рисуем кнопки
-        self.settings_list.draw()
-        self.escape_list.draw()
-        self.play_list.draw()
+        self.to_menu_list.draw()
+        self.restart_list.draw()
+        self.batch.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
         """Обработка клика мышью"""
-        # Проверяем нажатие на Play
-        if arcade.get_sprites_at_point((x, y), self.play_list):
+        if arcade.get_sprites_at_point((x, y), self.restart_list):
             self.restart()
             return
 
-        # Проверяем нажатие на Escape (выход)
-        if arcade.get_sprites_at_point((x, y), self.escape_list):
-            arcade.exit()
+        if arcade.get_sprites_at_point((x, y), self.to_menu_list):
+            self.application.show_menu()
             return
