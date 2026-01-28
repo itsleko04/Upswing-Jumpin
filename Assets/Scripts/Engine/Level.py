6b35ba109f877969b75ed465697a672cd720d6e0
itsleko04 <lipnitskiyvv@gmail.com>
2026-01-29 00:35:42 +0400

    Добавил след из частиц игроку-кубику

--- Assets/Scripts/Engine/Level.py
+++ Assets/Scripts/Engine/Level.py
@@ -1,11 +1,40 @@
 import arcade
-import datetime
+import random
+from arcade.particles import Emitter, EmitMaintainCount, FadeParticle
 from Assets.Scripts.Engine import Event
 from Assets.Scripts.Content.PlayerModes import PlayerCube
 from Assets.Scripts.Content.GameResultView import GameResultView
 
 from Assets.Scripts.Engine import InputSystem
 from Assets.GC import GRAVITY, CAMERA_LERP
+
+
+#region Система частиц
+SPARK_TEX = [
+    arcade.make_soft_circle_texture(16, arcade.color.ASH_GREY),
+    arcade.make_soft_circle_texture(16, arcade.color.COOL_GREY),
+    arcade.make_soft_circle_texture(16, arcade.color.BLUE_GRAY),
+    arcade.make_soft_circle_texture(16, arcade.color.DARK_GRAY),
+]
+
+
+def make_trail(attached_sprite, maintain=60):
+    # «След за объектом»: поддерживаем постоянное число частиц
+    emit = Emitter(
+        center_xy=(attached_sprite.center_x, attached_sprite.center_y),
+        emit_controller=EmitMaintainCount(maintain),
+        particle_factory=lambda e: FadeParticle(
+            filename_or_texture=random.choice(SPARK_TEX),
+            change_xy=arcade.math.rand_in_circle((0.0, 0.0), 1.6),
+            lifetime=random.uniform(0.15, 0.3),
+            start_alpha=220, end_alpha=0,
+            scale=random.uniform(0.25, 0.4),
+        ),
+    )
+    # Хитрость: каждое обновление будем прижимать центр к спрайту (см. ниже)
+    emit._attached = attached_sprite
+    return emit
+#endregion
 
 
 class Level(arcade.View):
@@ -26,12 +55,14 @@ class Level(arcade.View):
 
     def setup(self, tilemap_path):
         self.is_game_over = False
+
         self.player_list = arcade.SpriteList()
         self.player = PlayerCube(self.application.settings["Sprites"]["PlayerIdle"], 0.075)
         self.player.left = self.cell_size * 11
         self.player.bottom = self.cell_size * 11
         self.player.on_death.connect(self.on_game_over.invoke)
         self.player_list.append(self.player)
+        self.player_trail = make_trail(self.player)
         
         self.world_camera = arcade.camera.Camera2D()
         self.world_camera.position = self.player.position
@@ -50,12 +81,14 @@ class Level(arcade.View):
             gravity_constant=GRAVITY
         )
 
+        self.player_tail_timer = 0
         self.gameplay_time = 0
 
     def on_update(self, delta_time):
         if self.is_game_over:
             return
-        
+
+        self.player_tail_timer += delta_time
         self.gameplay_time += delta_time
         
         jump_keys = (InputSystem.Keys.SPACE, InputSystem.Keys.MOUSE_LEFT)
@@ -65,6 +98,11 @@ class Level(arcade.View):
         
         self.player_list.update(delta_time)
         self.physics_engine.update()
+
+        if self.player_trail:
+            self.player_trail.center_x = self.player.center_x
+            self.player_trail.center_y = self.player.bottom + 5
+            self.player_trail.update(delta_time)
 
         self.world_camera.position = arcade.math.lerp_2d(
             self.world_camera.position,
@@ -97,6 +135,8 @@ class Level(arcade.View):
                                self.window.rect.width, self.window.rect.height)
             arcade.draw_texture_rect(self.bg, rect)
         self.scene.draw()
+        if self.physics_engine.can_jump() and self.player_tail_timer > 0.75:
+            self.player_trail.draw()
         self.player_list.draw()
 
     def __on_game_over_flag(self):
@@ -107,8 +147,9 @@ class Level(arcade.View):
         self.window.show_view(GameResultView(self.application, "VICTORY!!!", "Уровень пройден!", self.gameplay_time))
 
     def __try_to_jump(self):
-        if self.physics_engine.can_jump(5) and self.player.change_y == 0 or self.player.can_double_jump:
+        if self.physics_engine.can_jump() and self.player.change_y == 0 or self.player.can_double_jump:
             self.player.change_y = 0
             self.player.jump()
+            self.player_tail_timer = 0
         if self.player.can_dash:
             self.player.dashing = True
